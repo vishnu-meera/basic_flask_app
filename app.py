73333a1c44a4c2a8eeb0e502274ada352fcfd5f7
Vishnu Sankar <vishgitt@gmail.com>
2019-11-03 02:44:25 -0800

    run.py

--- app.py
+++ app.py
@@ -13,12 +13,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'SX>6OnJ0b>Td"n|'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 # api.add_resource(Store, '/store/<string:name>')
@@ -26,7 +20,7 @@ jwt = JWT(app, authenticate, identity)  # /auth
 # api.add_resource(Item, '/item/<string:name>')
 # api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
-api.add_resource(EmotionAl, '/Emotion')
+api.add_resource(EmotionAl, '/emotion')
 
 if __name__ == '__main__':
     from db import db
