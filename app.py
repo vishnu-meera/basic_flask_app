e35b7d7ad106cb763f5795a7dfcab8012886d893
vishnu-meera <vishgitt@gmail.com>
2020-04-02 12:40:46 -0700

    data.db

--- app.py
+++ app.py
@@ -46,6 +46,11 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 if __name__ == "__main__":
     from db import db
     db.init_app(app)
